17228f3db955b475d4a01cfff3ac25f0d55f924d
Roman Omarov <romo@tuta.io>
2021-03-02 23:01:31 +0100

    schema for db

--- schema.sql
+++ schema.sql
@@ -1,11 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Erstellungszeit: 02. Mrz 2021 um 07:36
--- Server-Version: 8.0.23
--- PHP-Version: 8.0.2
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,30 +26,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `sites` (
   `url` text NOT NULL,
   `text` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `category` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Indizes der exportierten Tabellen
---
 
 --
 -- Indizes für die Tabelle `sites`
 --
 ALTER TABLE `sites`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `sites`
---
-ALTER TABLE `sites`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
